cd83461e4cc608e4da64f29b932453a6541bc8d9
Pradeep <pardeepyadav2914@gmail.com>
2019-11-06 09:04:48 +0530

    learning log app

---
+++ learning_logs/urls.py
@@ -0,0 +1,24 @@
+""" Default urls patterns for learning_logs  """
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+
+    # page that show all topics
+    path('topics/', views.topics, name='topics'),
+
+    # Details page for single topics
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # Page for adding new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # page for adding new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    #Page for editing an entry
+    path('edit_entry/<int:entry_id>/',views.edit_entry,name='edit_entry')
+]
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+""" define URL pattern for users """
+
+from django.urls import path, include
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Include default url
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register')
+]
